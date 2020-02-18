34673588e40ec260d27fdd75a929ef72c202c801
Zamreg <jmsd.jose.dias@gmail.com>
2020-02-18 19:41:57 +0000

    Testing handson table

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import router from './router'
+import Vuetify from 'vuetify'
 
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+  iconfont: 'mdi'
+})
 Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <HotTable :settings="hotSettings"/>
+  </v-container>
+</template>
+
+<script>
+import { HotTable } from '@handsontable/vue';
+import Handsontable from 'handsontable';
+export default {
+  name: 'tabletest',
+  components:{
+    HotTable
+  },
+  data: function() {
+    return {
+      hotSettings: {
+        data: Handsontable.helper.createSpreadsheetData(6, 10),
+        colHeaders: true,
+        licenseKey: 'non-commercial-and-evaluation'
+      }
+    }
+  },
+}
+</script>
+
+<style src="../../node_modules/handsontable/dist/handsontable.full.css"></style>
