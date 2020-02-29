c6fa24e845a6530ca905ef4d40cf8a66b5802d0f
Zamreg <jmsd.jose.dias@gmail.com>
2020-02-29 22:31:38 +0000

    1st model for stats page

---
+++ frontend/src/components/StatsBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card>
+   <v-card-title>{{title}}</v-card-title>
+    <v-row>
+     <v-col v-for="i in items" :key="i" >
+       <v-card outlined>
+        <v-card-subtitle>olá</v-card-subtitle>
+       </v-card>
+     </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props:{
+    title: String,
+    stats: Array
+  },
+  data() {
+    return{
+      items:[1,2,3,4,5]
+    }
+  }
+}
+</script>
--- frontend/src/views/Statistics.vue
+++ frontend/src/views/Statistics.vue
@@ -1,14 +1,21 @@
 <template>
   <v-container fluid>
     <Title conteudo="Statistics" class="display-1" />
+    <!--template v-for="item in statsheaders"-->
+      <StatsBar :key="item" title="Statsheaders"/>
+    <!--/template-->
   </v-container>
 </template>
 
 <script>
 import Title from '@/components/Title.vue'
+import StatsBar from '@/components/StatsBar.vue'
 export default {
   components:{
     Title,
-  }
+    StatsBar
+  },
+  props: ['statsheaders'],
+
 }
 </script>
